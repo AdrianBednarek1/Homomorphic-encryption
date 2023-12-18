d9f0d21c0f77aecb4feb7f0bc8e79b19dda46401
Adrian Bednarek <101347935+AdrianBednarek1@users.noreply.github.com>
2023-12-18 01:56:13 -0500

    Update server-client.py

--- Homomorphic encryption/server-client.py
+++ Homomorphic encryption/server-client.py
@@ -6,7 +6,7 @@ from Pyfhel import Pyfhel, PyCtxt, PyPtxt
 context = zmq.Context()
 #Creating server
 server = context.socket(zmq.REP)
-server.bind("tcp://*:5554")
+server.bind("tcp://192.168.100.5:5554")
 
 #Creating client
 client = context.socket(zmq.REQ)
@@ -18,22 +18,25 @@ client.send_string("Send message")
 
 jsonMessage = client.recv_json()
 print("Received message ")
-client.send_string("Received")#telling server that we received message
+#telling server that we received message
+client.send_string("Received")
 
 stringContext=json.loads(jsonMessage)
 stringCx=json.loads(jsonMessage)
 
 bajtContext=stringContext['context'].encode('cp437')
-#cx=stringContext['cx'].encode('cp437')
 
 HE_server = Pyfhel()
 HE_server.from_bytes_context(bajtContext)
 
 cx=PyCtxt(pyfhel=HE_server, bytestring=stringContext['cx'].encode('cp437'))
 
-nova_placa = cx + HE_server.encode(np.array([200]))
-nova_placa *= HE_server.encode(np.array([1.2]))
+print(f'Created PyCtxt')
 
+nova_placa = cx + HE_server.encode(np.array([200]))
+nova_placa *= HE_server.encode(np.array([1.5]))
+
+print(f'Prefomed transformation')
 #listening as server and sening rep
 while True:
     #receiving req
@@ -42,5 +45,5 @@ while True:
 
     time.sleep(1)
 
-    server.send(cx)
+    server.send_string(nova_placa.to_bytes().decode('cp437'))
     if(message=="Received"):break
