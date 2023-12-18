f773280445eb3d4271b61642ccdd9207ecf56942
Adrian Bednarek <101347935+AdrianBednarek1@users.noreply.github.com>
2023-12-18 01:55:27 -0500

    Update client-server.py

--- Homomorphic encryption/client-server.py
+++ Homomorphic encryption/client-server.py
@@ -2,7 +2,7 @@ import zmq, json, time, pickle, sys
 import numpy as np
 from Pyfhel import Pyfhel, PyCtxt, PyPtxt
 
-n_mults = 2
+n_mults = 3
 HE_client = Pyfhel(key_gen=True, context_params ={
     'scheme': 'CKKS',
     'n': 2**14,
@@ -21,10 +21,6 @@ cx = HE_client.encrypt(x)
 
 print("context to bytes")
 s_context    = HE_client.to_bytes_context()
-#radi bez toga
-#s_public_key = HE_client.to_bytes_public_key()
-#s_relin_key  = HE_client.to_bytes_relin_key()
-#s_rotate_key = HE_client.to_bytes_rotate_key()
 s_cx         = cx.to_bytes()
 
 #preparing json message for sending
@@ -32,19 +28,15 @@ stringCx= s_cx.decode('cp437')
 stringContext = s_context.decode('cp437')
 js=json.dumps({"context":stringContext,"cx":stringCx})
 
-print("Creating public keys")
-HE_server = Pyfhel()
-HE_server.from_bytes_context(s_context)
-
 #------CLIENTandSERVER------------------
 context = zmq.Context()
 #Creating server
 server = context.socket(zmq.REP)
-server.bind("tcp://192.168.100.5:5555")
+server.bind("tcp://192.168.100.6:5555")
 
 #Creating client
 client = context.socket(zmq.REQ)
-server.connect("tcp://localhost:5554")
+client.connect("tcp://192.168.100.7:5554")
 
 #listening as server and sening rep
 while True:
@@ -62,15 +54,13 @@ print("Sending request")
 client.send_string("Send message")
 
 #receiving encrypted message 
-jsonMessage = client.recv_json()
+jsonMessage = client.recv_string()
 print("Received message")
 client.send_string("Received")
 
 #decryption
-stringMessage = json.loads(jsonMessage)
-cxMessage= stringMessage['cx'].encode('utf-8')
+cxMessage= PyCtxt(pyfhel=HE_client, bytestring=jsonMessage.encode('cp437'))
 res = HE_client.decryptFrac(cxMessage)[0]
 
-print(f"[Client] Response received! Result is {np.round(res, 4)}, should be {1200 * 1.2}")
-
+print(f"[Client] Response received! Result is {np.round(res, 4)}, should be {1200 * 1.5}")
 
